86624c767e09c40b0b3afce6fe78b5b1c80d79da
zhamdi <zied@weally.org>
2023-11-28 00:16:27 +0100

    Added my contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 
 			<!-- Buttons -->
 			<div class="text-center">
-				<a class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800" href="offers/yearly">
+				<a href="offers/yearly" class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800" >
 					See Demo
 					<svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
 				</a>
@@ -39,5 +39,85 @@
 			<!-- End Buttons -->
 		</div>
 	</div>
+
+</div>
+<!-- End Hero -->
+
+
+
+<!-- Hero -->
+<div class="mt-12 max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+	<!-- Grid -->
+	<div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
+		<div>
+			<h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Dare <span class="text-blue-600">Attractive Offers</span> in your startup</h1>
+			<p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Expertly crafted pay-as-you-go system, designed for many kinds of startups. Database, stripe and even web components included.</p>
+
+			<!-- Buttons -->
+			<div class="mt-7 grid gap-3 w-full sm:inline-flex">
+				<a href="offers/yearly" class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+					Try the demo
+					<svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+				</a>
+				<a href='https://cvlink.in/cv/64a2bf631657ebcdbc710180' class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+					Contact me
+				</a>
+			</div>
+			<!-- End Buttons -->
+
+			<!-- Review -->
+			<div class="mt-1">
+				<!-- Toast -->
+				<div class="ml-0 sm:ml-40 md:ml-10 lg:ml-40 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700" role="alert">
+					<div class="flex p-4">
+						<div class="flex-shrink-0">
+							<svg class="flex-shrink-0 h-4 w-4 text-teal-500 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+								<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+							</svg>
+						</div>
+						<div class="ms-3">
+							<p class="text-sm text-gray-700 dark:text-gray-400">
+								I'm exploring opportunities for an accomplished developer specializing in Node.js and/or Java? Feel free to reach out — I am actively seeking engaging projects and collaborations. Let's connect!
+							</p>
+						</div>
+					</div>
+				</div>
+				<!-- End Toast -->
+			</div>
+			<!-- End Review -->
+		</div>
+		<!-- End Col -->
+
+		<div class="relative ms-4">
+			<img class="w-full rounded-md" src="https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&h=800&q=80" alt="Image Description">
+			<div class="absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 me-4 -ms-4 lg:mt-6 lg:-mb-6 lg:me-6 lg:-ms-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0"></div>
+
+			<!-- SVG-->
+			<div class="absolute bottom-0 start-0">
+				<svg class="w-2/3 ms-auto h-auto text-white dark:text-slate-900" width="630" height="451" viewBox="0 0 630 451" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect x="531" y="352" width="99" height="99" fill="currentColor"/>
+					<rect x="140" y="352" width="106" height="99" fill="currentColor"/>
+					<rect x="482" y="402" width="64" height="49" fill="currentColor"/>
+					<rect x="433" y="402" width="63" height="49" fill="currentColor"/>
+					<rect x="384" y="352" width="49" height="50" fill="currentColor"/>
+					<rect x="531" y="328" width="50" height="50" fill="currentColor"/>
+					<rect x="99" y="303" width="49" height="58" fill="currentColor"/>
+					<rect x="99" y="352" width="49" height="50" fill="currentColor"/>
+					<rect x="99" y="392" width="49" height="59" fill="currentColor"/>
+					<rect x="44" y="402" width="66" height="49" fill="currentColor"/>
+					<rect x="234" y="402" width="62" height="49" fill="currentColor"/>
+					<rect x="334" y="303" width="50" height="49" fill="currentColor"/>
+					<rect x="581" width="49" height="49" fill="currentColor"/>
+					<rect x="581" width="49" height="64" fill="currentColor"/>
+					<rect x="482" y="123" width="49" height="49" fill="currentColor"/>
+					<rect x="507" y="124" width="49" height="24" fill="currentColor"/>
+					<rect x="531" y="49" width="99" height="99" fill="currentColor"/>
+				</svg>
+			</div>
+			<!-- End SVG-->
+		</div>
+		<!-- End Col -->
+	</div>
+	<!-- End Grid -->
 </div>
 <!-- End Hero -->
